d9183ac9fe77fa2316bfe91f02c9b09d69ff448e
Johannes Klinger <johannesklinger@web.de>
2014-10-28 18:36:48 +0100

    Script as command line tool

--- tinder_image/tinder_images.py
+++ tinder_image/tinder_images.py
@@ -1,7 +1,28 @@
-import csv, urllib
-with open('photos.csv') as csvfile:
-    imagereader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for idx, row in enumerate(imagereader):
-        imgId= row[0].split(',')[1].strip('"')
-        usrId= row[0].split(',')[0]
-        urllib.urlretrieve("http://images.gotinder.com/"+imgId+"/"+usrId+".jpg",str(idx)+"_"+imgId+".jpg")
+#!/usr/bin/env python
+import csv, urllib, optparse
+
+def main():
+    tinderImageBaseUrl="http://images.gotinder.com"
+    parser = optparse.OptionParser()
+    
+    parser.add_option("-f", "--filename", metavar="FILENAME", help="CSV file with photo data")
+    (options, args) = parser.parse_args()
+    
+    if not options.filename:  
+        parser.error('Filename not given')
+
+    with open(options.filename) as csvfile:
+        imagereader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        
+        for idx, row in enumerate(imagereader):
+            rowSplitted=row[0].split(',')
+            imgId= rowSplitted[1].strip('"')
+            usrId= rowSplitted[0]
+            urllib.urlretrieve(tinderImageBaseUrl+"/"+imgId+"/"+usrId+".jpg",str(idx)+"_"+imgId+".jpg")
+            print '\rImage Number {0} retrieved'.format(idx),           
+         
+if __name__ == '__main__':
+    main()
+
+
+         
