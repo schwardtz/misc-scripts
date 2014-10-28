e58ac31f75f332ba38335ecc70c14d5a8546766e
Johannes Klinger <johannesklinger@web.de>
2014-10-28 21:40:31 +0100

    Changes to work with correctly exported csv files

--- tinder_image/tinder_images.py
+++ tinder_image/tinder_images.py
@@ -15,10 +15,9 @@ def main():
         imagereader = csv.reader(csvfile, delimiter=',', quotechar='"')
         
         for idx, row in enumerate(imagereader):
-            rowSplitted=row[0].split(',')
-            imgId= rowSplitted[1].strip('"')
-            usrId= rowSplitted[0]
-            urllib.urlretrieve(tinderImageBaseUrl+"/"+imgId+"/"+usrId+".jpg",str(idx)+"_"+imgId+".jpg")
+            usrId= row[1]
+            imgId= row[0]
+            urllib.urlretrieve(tinderImageBaseUrl+"/"+usrId+"/"+imgId+".jpg",usrId+"_"+imgId+".jpg")
             print '\rImage Number {0} retrieved'.format(idx),           
          
 if __name__ == '__main__':
